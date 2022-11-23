5d162f5cc4c15540a0b46e2679130f21b7294c35
Kim jinwoo <49385170+Sunny-jinn@users.noreply.github.com>
2022-11-23 14:57:30 +0900

    refactor: Image files upload

---
+++ src/assets/img/freshman.js
@@ -0,0 +1,12 @@
+import f1 from "./freshman/0.png";
+import f2 from "./freshman/1.png";
+import f3 from "./freshman/2.png";
+import f4 from "./freshman/3.png";
+import f5 from "./freshman/4.png";
+import f6 from "./freshman/5.png";
+import f7 from "./freshman/6.png";
+import f8 from "./freshman/7.png";
+
+const freshmanArr = [f1, f2, f3, f4, f5, f6, f7, f8];
+
+export default freshmanArr;
